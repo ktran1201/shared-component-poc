e9927464e18b25199cec5c13993cedd007d0ee11
Kevin Tran <kevin.tran.2@upstart.com>
2023-12-12 13:26:21 -0800

    components skeleton

---
+++ src/components/public/TextInput/TextInput.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface TextInputProps {
+
+}
+
+const TextInput: React.FC<TextInputProps> = () => {
+  return (
+    <h1>Hello from shared components</h1>
+  );
+};
+
+export default TextInput;
